75eb0936d7e29a5c317be12c1c20af0ab1df8a8c
John <jwbrooks0@gmail.com>
2019-02-02 13:28:45 -0500

    fixed bug

--- config1.py
+++ config1.py
@@ -6,12 +6,12 @@ This code is just a test of the functions of this library.
 # import libraries
 #import LCD2004 as lcd
 #import button
-import time
+#import time
 import m5e
 import RPi.GPIO as GPIO
 import pandas as pd
 #import numpy as np
-from AIMS import aimsLib
+from AIMS import aimsLib; reload(aimsLib)
 
 # instruct the RPi to ignore warnings
 GPIO.setwarnings(False)
@@ -19,7 +19,7 @@ GPIO.setwarnings(False)
 # initialize the LCD screen
 #lcd1=lcd.LCD()
 #lcd1.write_to_LCD(["","Awaiting","a command",""])
-#lcd1.toggle_backlight(True)
+#lcd1.toggle_backlight(False)
 
 # initialize the buttons
 #b1 = button.button(5)
@@ -27,7 +27,7 @@ GPIO.setwarnings(False)
 
 
 # initialize the M5e rfid reader
-reader = m5e.M5e()
+reader = m5e.M5e(verbosity=False)
 
 
 	
